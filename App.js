233e3b8c7a13cd709280da869566f9e15b8b9903
infinitybrad <seoih415@gmail.com>
2019-05-14 22:22:13 +0900

    second commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { AppLoading, Font } from 'expo';
+import { Ionicons } from "@expo/vector-icons";
+// import { StyleSheet, Text, View } from 'react-native';
+import TabNavigation from "./navigation/TabNavigation";
+
+
+
+export default class App extends React.Component {
+
+  state = {
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+
+  render() {
+    const { loaded } = this.state;
+    if (loaded) {
+      return <TabNavigation />;
+    } else {
+      return (
+          <AppLoading
+              startAsync={this.loadAssets}
+              onFinish={this.handleLoaded}
+              onError={this.handleError}
+          />
+      );
+    }
+
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
---
+++ navigation/TabNavigation.js
@@ -0,0 +1,23 @@
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import MovieScreen from "../screens/Movies";
+import TVScreen from "../screens/Search";
+import SearchScreen from "../screens/Search";
+import { BG_COLOR } from "../constants/Colors";
+
+const TabNavigation =  createBottomTabNavigator(
+    {
+        Movies: MovieScreen,
+        TV: TVScreen,
+        Search: SearchScreen
+    },
+    {
+        tabBarOptions: {
+            showLabel: true,
+            style: {
+                backgroundColor: BG_COLOR
+            }
+        }
+    }
+);
+
+export default createAppContainer(TabNavigation);
