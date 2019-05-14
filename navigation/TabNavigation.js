1d3609184685894c52820e8dad87c5079ea3a962
infinitybrad <seoih415@gmail.com>
2019-05-14 22:41:09 +0900

    add tabBarIcon

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,14 +1,50 @@
+import React from "react";
+import {Platform} from "react-native";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import MovieScreen from "../screens/Movies";
-import TVScreen from "../screens/Search";
+import TVScreen from "../screens/TV";
 import SearchScreen from "../screens/Search";
 import { BG_COLOR } from "../constants/Colors";
+import TabBarIcon from "../components/TabBarIcon";
 
 const TabNavigation =  createBottomTabNavigator(
     {
-        Movies: MovieScreen,
-        TV: TVScreen,
-        Search: SearchScreen
+        Movies: 
+        {
+            screen:MovieScreen,
+            navigationOptions:{
+                tabBarIcon:({focused})=>(
+                    <TabBarIcon 
+                        focused={focused}
+                        name={Platform.OS==="ios"?"ios-film":"md-film"}
+                    />
+                    
+                )
+            }
+
+        },
+        TV: {
+            screen: TVScreen,
+            navigationOptions: {
+                tabBarIcon: ({ focused }) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+                    />
+                )
+            }
+        },
+        Search: {
+            screen: SearchScreen,
+            navigationOptions: {
+                tabBarIcon: ({ focused }) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+                    />
+                )
+            }
+        }
     },
     {
         tabBarOptions: {
